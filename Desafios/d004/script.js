003c01d3fd532c3aa3f496037186ce9324cddc90
HenriqueCanhadas <pedrosacanhadas@gmail.com>
2023-09-28 11:17:02 -0300

    d004

---
+++ Desafios/d004/script.js
@@ -0,0 +1,19 @@
+function tabuada(){
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0){
+        alert('Por favor, digite um número!')
+    }
+    else{
+        var n =Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while (c<=10){
+            var item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}

e56a53a79f8f5c43bf2d8e87fff1a45a46f6c0ee
HenriqueCanhadas <pedrosacanhadas@gmail.com>
2023-09-28 10:42:22 -0300

    d003

--- Exercicios/ex014.js
+++ Exercicios/ex014.js
@@ -1,4 +1,5 @@
 console.log('Tudo Bem?')
+
 console.log('-----------')
 
 var c = 1
@@ -7,3 +8,13 @@ while(c<6){
     console.log('Tudo Bem?')
     c++
 }
+
+console.log('-----------')
+
+var c = 1
+
+do{
+    console.log('Tudo Bem?')
+    c++
+}while(c<6)
+
---
+++ Exercicios/ex015.js
@@ -0,0 +1,14 @@
+var c=1
+
+while ( c<= 5){
+    console.log(c)
+    c++
+}
+
+console.log('-----------------------')
+
+for( var c=1; c <=5 ; c++){
+    console.log(c)
+}
+
+console.log('-----------------------')
